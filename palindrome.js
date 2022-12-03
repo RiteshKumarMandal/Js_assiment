4a5a2f9174011a2b04efba46da35ba50e79c5602
RiteshKumarMandal <riteshkumarm260@gmail.com>
2022-12-03 10:11:15 +0530

    error_done

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let  str = "madam";
+let bag =0;
+for(let i=str.length-1;i<=0;i++){
+
+bga +=str[i]
+
+if(bag ==str){
+console.log((true){
+console.log(false)
---
+++ prime.js
@@ -0,0 +1,9 @@
+//Find prime number
+
+let x = 1,2,3,4,5,6,7,8,;
+
+if(x%2==0){
+console.log("prime number");
+}else{
+console.log("No prime number");
+}
