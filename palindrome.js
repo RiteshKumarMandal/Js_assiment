52a948961e1d3e02f1d11be6e87d0b086368e595
Shashwat2104 <Shashwatmahender2104@gmail.com>
2022-12-04 18:26:58 +0530

    done

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,10 @@
 let  str = "madam";
-let bag =0;
-for(let i=str.length-1;i<=0;i++){
+let bag = ""
+for(let i=str.length-1;i<=0;i--){
 
 bga +=str[i]
 
-if(bag ==str){
-console.log((true){
+if(bag == str){
+console.log(true)}else{
 console.log(false)
+}
